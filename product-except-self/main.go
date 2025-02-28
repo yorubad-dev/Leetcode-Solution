2c64402eb0b518e8a5e0ddec1f68d7121df75ebf
yorubad-dev <adetoyeanointing15@gmail.com>
2025-02-28 17:59:28 +0100

    product except self

---
+++ product-except-self/main.go
@@ -0,0 +1,23 @@
+package main
+
+func productExceptSelf(nums []int) []int {
+    res := make([]int, len(nums))
+
+    prefix := 1
+    for i, num := range nums {
+        res[i] = prefix
+        prefix *= num
+    }
+
+    postfix := 1
+    for i := len(nums) - 1; i >= 0; i-- {
+        res[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return res
+}
+
+func main() {
+	productExceptSelf([]int{1, 2, 3, 4})
+}
